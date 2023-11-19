33abaa15a5453bc631974e6532e4c24f2006daab
Christian Eddie <122130586+Eddie1288@users.noreply.github.com>
2023-11-18 21:30:23 -0700

    Hello

---
+++ app/src/main/java/com/example/purchasepal/MainMenuActivity.java
@@ -0,0 +1,16 @@
+package com.example.purchasepal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.purchasepal.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
